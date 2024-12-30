2a0ce28147fda9d2582edb11f0811f044b4c1534
ze97286 <zohar.etzioni@gmail.com>
2024-12-30 14:53:43 +0000

    chore: pats generation script

--- src/deep_conv/atlas/generate_pats.sh
+++ src/deep_conv/atlas/generate_pats.sh
@@ -12,6 +12,9 @@ fi
 mkdir -p $PATDIR
 mkdir -p $TMPDIR
 
-parallel -j $THREADS "if [ ! -e $PATDIR/{/} ]; then tabix -R <(tail -n+2 $MARKERBED | cut -f 1,4,5) {} | sort -k1,1V -k2,2n -k3,3 | bgzip -c > $PATDIR/{/}; tabix -s 1 -b 2 -e 2 -C $PATDIR/{/}; fi" ::: `cut -f 2 $BASEDIR/type2bam.tsv | sed 's:/mnt/lustre/users/ramess/TAPSbeta_tissue_map/Results/1.3.1/Alignments/:/mnt/lustre/users/bschuster/Tissue_map/Results/1.2/pat:' | sed 's/_md.bam/.pat.gz/'`
+# First get input files
+input_files=$(cut -f 2 $BASEDIR/type2bam.tsv | sed 's:/mnt/lustre/users/ramess/TAPSbeta_tissue_map/Results/1.3.1/Alignments/:/mnt/lustre/users/bschuster/Tissue_map/Results/1.2/pat:' | sed 's/_md.bam/.pat.gz/')
+
+parallel -j $THREADS "if [ ! -e $PATDIR/\$(basename {}) ]; then tabix -R <(tail -n+2 $MARKERBED | cut -f 1,4,5) {} | sort -k1,1V -k2,2n -k3,3 | bgzip -c > $PATDIR/\$(basename {}); tabix -s 1 -b 2 -e 2 -C $PATDIR/\$(basename {}); fi" ::: $input_files
 
 rm -rf $TMPDIR
