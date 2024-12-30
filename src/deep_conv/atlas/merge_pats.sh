80258fcfe47df0661f22b935690bea754070dea5
ze97286 <zohar.etzioni@gmail.com>
2024-12-30 15:18:18 +0000

    chore: pats generation script

--- src/deep_conv/atlas/merge_pats.sh
+++ src/deep_conv/atlas/merge_pats.sh
@@ -7,15 +7,15 @@ MAPFILE=$BASEDIR/type2bam.tsv
 
 mkdir -p $PATDIR
 
-# Get unique cell types and properly quote them
-celltypes=$(cut -f 1 $MAPFILE | sort -u | sed "s/.*/'&'/")
-
-parallel -j $THREADS "if [ ! -e $PATDIR/{/.}.pat.gz ]; then 
-    zcat \$(fgrep -w {} $MAPFILE | cut -f 2 | 
-    sed 's:.*/:$SOURCEDIR/:' | 
-    sed 's/_md.bam/.pat.gz/') | 
-    sort -k1,1V -k2,2n -k3,3 | 
-    perl -n deduplicate_pat.pl | 
-    bgzip -c > $PATDIR/{/.}.pat.gz; 
-    tabix -s 1 -b 2 -e 2 -C $PATDIR/{/.}.pat.gz; 
-fi" ::: $celltypes
+# Process each cell type separately
+cut -f 1 $MAPFILE | sort -u | while read celltype; do
+    if [ ! -e "$PATDIR/${celltype}.pat.gz" ]; then
+        zcat $(fgrep -w "$celltype" $MAPFILE | cut -f 2 | \
+            sed "s:.*/:$SOURCEDIR/:" | \
+            sed 's/_md.bam/.pat.gz/') | \
+            sort -k1,1V -k2,2n -k3,3 | \
+            perl -n deduplicate_pat.pl | \
+            bgzip -c > "$PATDIR/${celltype}.pat.gz"
+        tabix -s 1 -b 2 -e 2 -C "$PATDIR/${celltype}.pat.gz"
+    fi
+done
