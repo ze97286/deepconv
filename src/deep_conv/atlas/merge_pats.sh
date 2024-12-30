cdcada7803cb41aa21a7698b9068394c22a87f70
ze97286 <zohar.etzioni@gmail.com>
2024-12-30 15:15:08 +0000

    chore: pats generation script

--- src/deep_conv/atlas/merge_pats.sh
+++ src/deep_conv/atlas/merge_pats.sh
@@ -1,4 +1,4 @@
-# src/deep_conv/atlas/merge_pats.sh /users/zetzioni/sharedscratch/pat/dmr_by_read.blood+gi+tum.250/Song_pattools/files /users/zetzioni/sharedscratch/pat/dmr_by_read.blood+gi+tum.250/Song_pattools/celltypes
+# /users/zetzioni/sharedscratch/deepconv/src/deep_conv/atlas/merge_pats.sh /users/zetzioni/sharedscratch/pat/dmr_by_read.blood+gi+tum.250/Song_pattools/files /users/zetzioni/sharedscratch/pat/dmr_by_read.blood+gi+tum.250/Song_pattools/celltypes
  
 SOURCEDIR=$1
 PATDIR=$2
@@ -7,4 +7,15 @@ MAPFILE=$BASEDIR/type2bam.tsv
 
 mkdir -p $PATDIR
 
-parallel -j $THREADS "if [ ! -e $PATDIR/{}.pat.gz ]; then zcat \$(fgrep -w {} $MAPFILE | cut -f 2 | sed 's:.*/:$SOURCEDIR/:' | sed 's/_md.bam/.pat.gz/') | sort -k1,1V -k2,2n -k3,3 | perl -n deduplicate_pat.pl | bgzip -c > $PATDIR/{}.pat.gz; tabix -s 1 -b 2 -e 2 -C $PATDIR/{}.pat.gz; fi" ::: `cut -f 1 $MAPFILE | sort -u`
+# Get unique cell types and properly quote them
+celltypes=$(cut -f 1 $MAPFILE | sort -u | sed "s/.*/'&'/")
+
+parallel -j $THREADS "if [ ! -e $PATDIR/{/.}.pat.gz ]; then 
+    zcat \$(fgrep -w {} $MAPFILE | cut -f 2 | 
+    sed 's:.*/:$SOURCEDIR/:' | 
+    sed 's/_md.bam/.pat.gz/') | 
+    sort -k1,1V -k2,2n -k3,3 | 
+    perl -n deduplicate_pat.pl | 
+    bgzip -c > $PATDIR/{/.}.pat.gz; 
+    tabix -s 1 -b 2 -e 2 -C $PATDIR/{/.}.pat.gz; 
+fi" ::: $celltypes
