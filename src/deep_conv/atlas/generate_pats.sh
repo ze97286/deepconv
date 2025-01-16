9f94ad5e15ebefb10c2135a04eed7403bdcdef06
ze97286 <zohar.etzioni@gmail.com>
2025-01-16 16:53:05 +0000

    chore: fix pat mixing

--- src/deep_conv/atlas/generate_pats.sh
+++ src/deep_conv/atlas/generate_pats.sh
@@ -16,6 +16,7 @@ mkdir -p $TMPDIR
 
 # Process each cell type from the mapping file
 cut -f 1 $TYPE2BAM | sort -u | while read celltype; do
+    echo "Processing cell type: $celltype"
     outfile="$PATDIR/${celltype}.pat.gz"
     pat_file="${INPUT_DIR}/${celltype}_merged.pat.gz"
     
@@ -26,11 +27,28 @@ cut -f 1 $TYPE2BAM | sort -u | while read celltype; do
             continue
         fi
         
-        tabix -R <(tail -n+2 $MARKERBED | awk -v OFS="\t" '{start=$4-2; end=$5-4; if (start<0) start=0; print $1,start,end}') "$pat_file" | \
+        echo "Creating regions file for tabix..."
+        regions_file=$TMPDIR/${celltype}_regions.bed
+        tail -n+2 $MARKERBED | awk -v OFS="\t" '{start=$4-2; end=$5-4; if (start<0) start=0; print $1,start,end}' > $regions_file
+        
+        echo "Running tabix for $celltype..."
+        tabix -R $regions_file "$pat_file" | \
             sort -k1,1V -k2,2n -k3,3 | \
             bgzip -c > "$outfile"
             
+        tabix_exit=$?
+        if [ $tabix_exit -ne 0 ]; then
+            echo "Error: tabix command failed for $celltype" 2>&1
+            continue
+        fi
+        
+        echo "Indexing output file..."
         tabix -s 1 -b 2 -e 2 -C "$outfile"
+        
+        # Clean up
+        rm $regions_file
+    else
+        echo "Output file already exists: $outfile"
     fi
 done
 
