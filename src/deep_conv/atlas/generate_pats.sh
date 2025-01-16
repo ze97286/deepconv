157c83ea21a4e4d4f91579b31107219d131223c6
ze97286 <zohar.etzioni@gmail.com>
2025-01-16 16:43:56 +0000

    chore: fix pat mixing

--- src/deep_conv/atlas/generate_pats.sh
+++ src/deep_conv/atlas/generate_pats.sh
@@ -26,7 +26,7 @@ cut -f 1 $TYPE2BAM | sort -u | while read celltype; do
             continue
         fi
         
-        tabix -R <(tail -n+2 $MARKERBED | awk -v OFS="\t" '{start=$4-2; end=$5+2; if (start<0) start=0; print $1,start,end}') "$pat_file" | \
+        tabix -R <(tail -n+2 $MARKERBED | awk -v OFS="\t" '{start=$4-2; end=$5-4; if (start<0) start=0; print $1,start,end}') "$pat_file" | \
             sort -k1,1V -k2,2n -k3,3 | \
             bgzip -c > "$outfile"
             
