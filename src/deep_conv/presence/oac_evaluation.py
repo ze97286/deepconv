63a49eb6f62ecee71563a75b91e5b5b19b6ceed6
ze97286 <zohar.etzioni@gmail.com>
2025-03-12 06:59:02 +0000

    chore: fix duplicate markers

--- src/deep_conv/presence/oac_evaluation.py
+++ src/deep_conv/presence/oac_evaluation.py
@@ -8,8 +8,8 @@ def train_and_evaluate(model_name, target_cell_type_name, use_loyfer=True):
     if use_loyfer:
         atlas_dir = "loyfer_atlas"
     atlas_path = f"/users/zetzioni/sharedscratch/{atlas_dir}/atlas/atlas_oac.blood+gi+tum.l4.bed"
-    train_pat_dir = f"/users/zetzioni/sharedscratch/{atlas_dir}/training/oac.blood+gi+tum.l4/train_high"
-    eval_pat_dir = f"/users/zetzioni/sharedscratch/{atlas_dir}/training/oac.blood+gi+tum.l4/eval_high"
+    train_pat_dir = f"/users/zetzioni/sharedscratch/{atlas_dir}/training/oac.blood+gi+tum.l4/train_high/"
+    eval_pat_dir = f"/users/zetzioni/sharedscratch/{atlas_dir}/training/oac.blood+gi+tum.l4/eval_high/"
 
     threads = 32
     output_path = Path(f"/users/zetzioni/sharedscratch/{atlas_dir}/saved_models/{model_name}/")
