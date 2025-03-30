cbdc4362f69c55df8321fef9ac31d20e9d911fc5
ze97286 <zohar.etzioni@gmail.com>
2025-03-30 13:00:51 +0100

    chore: model overhaul

--- src/deep_conv/deconvolution/deepconv.py
+++ src/deep_conv/deconvolution/deepconv.py
@@ -641,8 +641,7 @@ def test_augmentation(train_pat_dir, atlas, names, target_dist_params=None):
     aug_X_train, aug_coverage_train = coverage_matched_augmentation(
         X_train, 
         coverage_train, 
-        target_dist_params=target_dist_params,
-        augmentation_probability=1.0  # Apply to all samples for testing
+        augmentation_prob=1.0  
     )
     
     # Create the visualization
