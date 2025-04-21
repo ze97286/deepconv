c6735495b8bce0c57b7263be5caeaff3991423b5
ze97286 <zohar.etzioni@gmail.com>
2025-04-21 21:47:31 +0100

    chore: check precomputed with fresh model vs trained model

--- src/deep_conv/deconvolution/oac_evaluation_loyfer.py
+++ src/deep_conv/deconvolution/oac_evaluation_loyfer.py
@@ -570,7 +570,9 @@ def deepconv_estimate(atlas_path, eval_pat_dir, dilutions, presence_model_name, 
     num_cell_types = len(cell_types)
     num_samples = len(X_val)
     presence_probs = torch.zeros(num_samples, num_cell_types)
-    temp_dataset = TensorDataset(X_val, coverage_val)
+    X_val_tensor = torch.tensor(X_val, dtype=torch.float32)
+    coverage_val_tensor = torch.tensor(coverage_val, dtype=torch.float32)
+    temp_dataset = TensorDataset(X_val_tensor, coverage_val_tensor)
     temp_loader = DataLoader(temp_dataset, batch_size=batch_size, shuffle=False)
     with torch.no_grad():
         for batch_idx, (batch_fraction, batch_coverage) in enumerate(temp_loader):
