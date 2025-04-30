e787c8836657558d96f1b95d79adc7c57f05150a
ze97286 <zohar.etzioni@gmail.com>
2025-04-30 15:43:04 +0100

    chore: simplify oac

--- src/deep_conv/detect/train.py
+++ src/deep_conv/detect/train.py
@@ -67,7 +67,7 @@ def parse_args():
     # Misc parameters
     parser.add_argument('--seed', type=int, default=42, help='Random seed')
     parser.add_argument('--device', type=str, default='', help='Device to use (empty for auto)')
-    parser.add_argument('--save_interval', type=int, default=50, help='Save checkpoint every N epochs')
+    parser.add_argument('--save_interval', type=int, default=10, help='Save checkpoint every N epochs')
     
     parser.add_argument('--control_data_dir', type=str, default=None, 
                    help='Directory containing control data for contrastive learning')
