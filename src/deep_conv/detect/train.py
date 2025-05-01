d191d2ac235ea40b972f53f8cb34bbecd12b55dc
ze97286 <zohar.etzioni@gmail.com>
2025-05-01 15:20:17 +0100

    chore: logging

--- src/deep_conv/detect/train.py
+++ src/deep_conv/detect/train.py
@@ -508,10 +508,9 @@ def train_model(model, train_loader, val_loader, control_loader, args, device):
                 # Get model predictions
                 with autocast():
                     mu, uncertainty, detection_probs, _ = model(marker_values, coverage)
-                    if i==0:
-                        print(mu)
-                        print(uncertainty)
-                        print(detection_probs)
+                    print(mu)
+                    print(uncertainty)
+                    print(detection_probs)
                     
                     # Calculate combined loss
                     loss, conc_loss, det_loss = calculate_loss(
