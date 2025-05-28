2f2d49f5e82f6830191b924e4547149584abd5b6
ze97286 <zohar.etzioni@gmail.com>
2025-05-28 10:31:48 +0100

    chore: enhance merge aug

--- src/deep_conv/atlasbuilder/collect_markers_for_training.py
+++ src/deep_conv/atlasbuilder/collect_markers_for_training.py
@@ -162,24 +162,45 @@ def merge(base_dir, num_files, prefix, cov):
 
 
 def merge_aug(base_dir, num_files):
+	aug_markers = []
+	aug_coverage = []
+	aug_y = []
 	markers = []
 	coverage = []
 	y = []
+	metadata = []         
 	suffixes = [f"_batch{i}" for i in range(1,num_files+1)]
 	for i in range(1,num_files+1):		
-		markers.append(pd.read_parquet(base_dir+str(i)+"/aug_marker_values.parquet"))
-		coverage.append(pd.read_parquet(base_dir+str(i)+"/aug_coverage.parquet"))
-		y.append(pd.read_parquet(base_dir+str(i)+"/aug_ground_truth_y.parquet"))		
+		aug_markers.append(pd.read_parquet(base_dir+f"{str(i)}_aug_marker_values.parquet"))
+		aug_coverage.append(pd.read_parquet(base_dir+f"{str(i)}_aug_coverage.parquet"))
+		aug_y.append(pd.read_parquet(base_dir+f"{str(i)}_aug_ground_truth_y.parquet"))
+		metadata.append(pd.read_parquet(base_dir+f"{str(i)}_aug_sample_info.parquet"))		
+		markers.append(pd.read_parquet(base_dir+f"{str(i)}_marker_values.parquet"))
+		coverage.append(pd.read_parquet(base_dir+f"{str(i)}_coverage.parquet"))
+		y.append(pd.read_parquet(base_dir+f"{str(i)}_ground_truth_y.parquet"))
+	aug_merged_markers = aug_markers[0]
+	for i, m in enumerate(aug_markers[1:]):
+		aug_merged_markers = aug_merged_markers.merge(m, on=['name', 'direction'], how='outer',suffixes=('', suffixes[i]))
+	aug_merged_markers.to_parquet(f"{base_dir}/marker_values.parquet", index=False)
 	merged_markers = markers[0]
 	for i, m in enumerate(markers[1:]):
 		merged_markers = merged_markers.merge(m, on=['name', 'direction'], how='outer',suffixes=('', suffixes[i]))
+	merged_markers.to_parquet(f"{base_dir}/raw_marker_values.parquet", index=False)
+	aug_merged_coverage = aug_coverage[0]
+	for i, c in enumerate(aug_coverage[1:]):
+		aug_merged_coverage = aug_merged_coverage.merge(c, on=['name', 'direction'], how='outer',suffixes=('', suffixes[i]))
+	aug_merged_coverage.to_parquet(f"{base_dir}/coverage.parquet", index=False)
 	merged_coverage = coverage[0]
 	for i, c in enumerate(coverage[1:]):
 		merged_coverage = merged_coverage.merge(c, on=['name', 'direction'], how='outer',suffixes=('', suffixes[i]))
+	merged_coverage.to_parquet(f"{base_dir}/raw_coverage.parquet", index=False)
+	for i in range(num_files):
+		metadata[i]["original_index"] = metadata[i]["original_index"] + len(y[0])*i
+	metadata = pd.concat(metadata, ignore_index=True).fillna(0)
+	aug_y = pd.concat(aug_y, ignore_index=True).fillna(0)
+	aug_y.to_parquet(f"{base_dir}/ground_truth_y.parquet", index=False)
 	y = pd.concat(y, ignore_index=True).fillna(0)
-	merged_markers.to_parquet(f"{base_dir}/marker_values.parquet", index=False)
-	merged_coverage.to_parquet(f"{base_dir}/coverage.parquet", index=False)
-	y.to_parquet(f"{base_dir}/ground_truth_y.parquet", index=False)
+	y.to_parquet(f"{base_dir}/raw_ground_truth_y.parquet", index=False)
 	print(f"saved data to {base_dir}")
 
 
